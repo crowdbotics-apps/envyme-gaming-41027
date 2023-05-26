72ab0b7e33b3c736b43dfcc550d3f4e0d4167748
Crowdbotics <team@crowdbotics.com>
2023-05-26 21:37:15 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "envyme-gaming-41027",
+  url: "https://envyme-gaming-41027.botics.co",
+  api: "https://envyme-gaming-41027.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
